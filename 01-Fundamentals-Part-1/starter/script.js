4a7501a902f9dab31d52742d08d1d24e616d2922
perpyderp <g4m3rall3n@gmail.com>
2023-04-14 21:19:25 -0400

    Created folders and files with starting code for all sections

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,10 @@
+/*
+////////////////////////////////////
+// Linking a JavaScript File
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+////////////////////////////////////
+*/
+
+console.log("Jonas");
